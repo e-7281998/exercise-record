369db653c76c5b8e09d79484e9e44929c38eefbf
e-7281998 <jejjcs43@naver.com>
2022-06-03 23:22:43 +0900

    HeaderInfo 파일 위치 수정

--- src/pages/ListPage.js
+++ src/pages/ListPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import HeaderInfo from "../common/headerInfo";
+import HeaderInfo from "../component/common/headerInfo"
 import ShowExerciseList from "../component/ListPage/showExerciseList";
 
 const ListPage = () => {
@@ -9,6 +9,7 @@ const ListPage = () => {
             <HeaderInfo />
             <nav>
                 <a href="/write">기록하기</a>
+                <a href="/modify_info">정보수정</a>
             </nav>
             <ShowExerciseList />
         </>
