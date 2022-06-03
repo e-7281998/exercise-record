f0dab9f869ac4d1a2ce90b4bd087f935ec688fc7
e-7281998 <jejjcs43@naver.com>
2022-06-03 14:57:12 +0900

    메모 펼치기, /write 이동

--- src/pages/ListPage.js
+++ src/pages/ListPage.js
@@ -1,4 +1,9 @@
 import React from "react";
+import styled from "styled-components";
+
+const P = styled.p`
+    display: none;
+    `
 
 const ListPage = () => {
     //로컬스토리지에서 정보 불러오기
@@ -8,23 +13,37 @@ const ListPage = () => {
     const today = new Date();
     const day = today.getTime() - JSON.parse(localStorage.getItem('start'));
     const passed = Math.round(day / (1000 * 60 * 60 * 24)) + 1;
-    const recordList = JSON.parse(localStorage.getItem('recordList')) || [];
+    const recordList = JSON.parse(localStorage.getItem('recordList')).reverse() || [];
+
+    //메모 보여주기 - 버튼 클릭시
+    function showMemo(e) {
+        const memo = document.getElementsByTagName("P");
+        const styleState = window.getComputedStyle(memo[e.target.value]).getPropertyValue('display');
+        if (styleState == 'none') {
+            memo[e.target.value].style.display = 'block';
+        } else {
+            memo[e.target.value].style.display = 'none';
+        }
+    }
 
     return (
         <>
             <div>
                 {name}님! "{goal}"을 시작한지 {passed}일 째 입니다.
-                <ul>
-                    {recordList.map((list, n) => (
-                        <li key={n}>
-                            <span>{list.EXERCISE}</span>
-                            <span>{list.DAY}</span>
-                            <button>메모 펼치기</button>
-                            <p>{list.INFO}</p>
-                        </li>
-                    ))}
-                </ul>
             </div>
+            <div>
+                <a href="/write">기록하기</a>
+            </div>
+            <ul>
+                {recordList.map((list, n) => (
+                    <li key={n}>
+                        <span>{list.EXERCISE}</span>
+                        <span>{list.DAY}</span>
+                        <button value={n} onClick={showMemo}>메모 펼치기</button>
+                        <P>{list.INFO}</P>
+                    </li>
+                ))}
+            </ul>
         </>
 
     )
