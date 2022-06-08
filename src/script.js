dbfa4df880673ea35d220fd0f415ec98bd33823a
e-7281998 <jejjcs43@naver.com>
2022-06-08 18:23:47 +0900

    기록 하나씩 보이기, 목표 색상 바꾸기 에러 수정(info에서는 멈추기)

--- src/script.js
+++ src/script.js
@@ -3,6 +3,7 @@ import mainImg from './img/운동_메인.jpg';
 const body = document.getElementsByTagName('body');
 export const windowH = window.innerHeight;
 const goal = document.getElementsByClassName('goal');
+const ExerciseList = document.getElementsByTagName('li');
 
 body[0].style = `
     width: 100%; 
@@ -14,11 +15,34 @@ body[0].style = `
      text-align: center;
 `;
 
+const pathName = window.location.pathname;
+
 //목표 색상 바꾸기
-function goalColorChange() {
-    setInterval(function () {
-        var goalColor = goal[0].style.color;
-        goalColor === 'rgb(51, 51, 51)' ? goal[0].style = 'color: rgb(255, 0, 0);' : goal[0].style = 'color: rgb(51, 51, 51);';
-    }, 1000);
+const GoalColor = setInterval(function () {
+    var goalColor = goal[0].style.color;
+    goalColor === 'rgb(51, 51, 51)' ? goal[0].style = 'color: rgb(255, 0, 0);' : goal[0].style = 'color: rgb(51, 51, 51);';
+}, 1000);
+
+export function goalColorChange(value) {
+    switch (value) {
+        case 'start':
+            GoalColor:
+            break;
+        case 'stop':
+            clearInterval(GoalColor);
+            break;
+    }
 };
-goalColorChange();
+
+//기록 하나씩 보여주기
+export function showList() {
+    var i = 0;
+    const showListInverval = setInterval(function () {
+        if (i < ExerciseList.length) {
+            ExerciseList[i].classList.add('show');
+            i++;
+        }
+        else
+            clearInterval(showListInverval);
+    }, 500);
+}
